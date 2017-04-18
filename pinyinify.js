8c9f109515650d9ea23000ef40ecf5875080d0be
Kevin Yang <yangkevi@usc.edu>
2017-04-17 23:29:49 -0700

    barely started, added html page for testing

--- pinyinify.js
+++ pinyinify.js
@@ -1,4 +1,19 @@
 
+var isAlpha = function(char) {
+	return /^[A-Za-z]$/.test(char);
+}
+
 var pinyinify = function(str) {
 
+	var res = "";
+	for (var i = 0; i < str.length; i++) {
+		var char = str.charAt(i);
+		if (isAlpha(char)) {
+			//TODO look for index of tone number
+		} else {
+			res += char;
+		}
+	}
+
+	return res;
 }
